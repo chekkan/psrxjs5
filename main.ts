9cd9e3c2bd60574626a0f45da6280cd0f58fa58b
Harish Babu <harish.babu@creditsafe.com>
2018-07-20 11:23:01 +0100

    retry strategy

--- main.ts
+++ main.ts
@@ -10,19 +10,44 @@ let click = Observable.fromEvent(button, 'click')
 	.delay(300);
 
 function load(url: string) {
-	let xhr = new XMLHttpRequest();
+	return Observable.create((observer) => {
+		let xhr = new XMLHttpRequest();
 
-	xhr.addEventListener('load', () => {
-		let movies = JSON.parse(xhr.responseText);
-		movies.forEach((m) => {
-			let div = document.createElement('div');
-			div.innerText = m.title;
-			output.appendChild(div);
+		xhr.addEventListener('load', () => {
+			if (xhr.status === 200) {
+				let data = JSON.parse(xhr.responseText);
+				observer.next(data);
+				observer.complete();
+			} else {
+				observer.error(xhr.statusText);
+			}
 		});
-	});
 
-	xhr.open('GET', url);
-	xhr.send();
+		xhr.open('GET', url);
+		xhr.send();
+	}).retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
 }
 
-click.subscribe((e) => load('movies.json'), (e) => console.log(`error: ${e}`), () => console.log('complete'));
+function retryStrategy({ attempts = 4, delay = 1000 }) {
+	return function(errors) {
+		return errors
+			.scan((acc, value) => {
+				console.log(acc, value);
+				return acc + 1;
+			}, 0)
+			.takeWhile((acc) => acc < attempts)
+			.delay(delay);
+	};
+}
+
+function renderMovies(movies) {
+	movies.forEach((m) => {
+		let div = document.createElement('div');
+		div.innerText = m.title;
+		output.appendChild(div);
+	});
+}
+
+click
+	.flatMap((e) => load('moviess.json'))
+	.subscribe(renderMovies, (e) => console.log(`error: ${e}`), () => console.log('complete'));
