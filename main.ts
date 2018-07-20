3b707b933729a7c7727ea6be0d6ce8df13de2967
Harish Babu <harish.babu@creditsafe.com>
2018-07-20 14:45:53 +0100

    module completed

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs';
+import { load, loadWithFetch } from './loader';
 
 let output = document.getElementById('output');
 let button = document.getElementById('button');
@@ -9,43 +10,6 @@ let click = Observable.fromEvent(button, 'click')
 	.filter((value) => value.x < 500)
 	.delay(300);
 
-function load(url: string) {
-	return Observable.create((observer) => {
-		let xhr = new XMLHttpRequest();
-
-		xhr.addEventListener('load', () => {
-			if (xhr.status === 200) {
-				let data = JSON.parse(xhr.responseText);
-				observer.next(data);
-				observer.complete();
-			} else {
-				observer.error(xhr.statusText);
-			}
-		});
-
-		xhr.open('GET', url);
-		xhr.send();
-	}).retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
-}
-
-function loadWithFetch(url: string) {
-	return Observable.defer(() => {
-		return Observable.fromPromise(fetch(url).then((r) => r.json()));
-	});
-}
-
-function retryStrategy({ attempts = 4, delay = 1000 }) {
-	return function(errors) {
-		return errors
-			.scan((acc, value) => {
-				console.log(acc, value);
-				return acc + 1;
-			}, 0)
-			.takeWhile((acc) => acc < attempts)
-			.delay(delay);
-	};
-}
-
 function renderMovies(movies) {
 	movies.forEach((m) => {
 		let div = document.createElement('div');
@@ -53,6 +17,15 @@ function renderMovies(movies) {
 		output.appendChild(div);
 	});
 }
+
+let subscription = load('moviess.json').subscribe(
+	renderMovies,
+	(e) => console.log(`error: ${e}`),
+	() => console.log('complete!')
+);
+
+console.log(subscription);
+// subscription.unsubscribe();
 
 click
 	.flatMap((e) => loadWithFetch('movies.json'))
